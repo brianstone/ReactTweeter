fc31452258891b7a041ea81d24e8ea5a6e5f3be7
Brian Stone <brianpatrickstone@gmail.com>
2016-08-25 19:15:13 -0600

    fixing api issues

--- src/App.js
+++ src/App.js
@@ -1,29 +1,48 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
-import { fetchTweets } from './Lib/Api'
-import _ from 'lodash';
+// import { fetchTweets } from './Lib/Api'
+import axios from 'axios';
 
+// import _ from 'lodash';
+function fetchTweets() {
+  console.log("FOOOfoo")
+  axios.get('https://safe-headland-90182.herokuapp.com/')
+    .then(function(response) {
+      console.log(response);
+      console.log(response.status);
+      return response.data;
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+}
 
-class App extends Component {
+class App extends React.Component {
   constructor(props) {
     super(props);
+    // this.tweets = this.tweets.bind(this);
     this.state = {data: null};
   }
 
   componentDidMount() {
+    console.log("FooBar");
+    // console.log(fetchTweets());
     fetchTweets().then((response) => {
      this.setState({data: response})
     });
   }
 
   tweets() {
-    _.map(this.state.data, (tweet) => {
+    console.log(this.state)
+    return this.state.data.map((tweet) => {
       return <div>{tweet.user}: {tweet.text}</div>
     });
   }
 
   render() {
+    // fetchTweets();
     // console.log(this.state.data)
+    const {data} = this.state;
     return (
       <div className="App">
         <div className="tweeter-header">
@@ -31,7 +50,9 @@ class App extends Component {
           <span>#</span><input type="text" />
         </div>
         <h1>Tweets</h1>
-        {this.tweets()}
+        {data && data.map((tweet) => (
+          <div>{tweet.user}: {tweet.text}</div>
+        ))}
       </div>
     );
   }
--- src/Lib/Api.js
+++ src/Lib/Api.js
@@ -1,11 +1,15 @@
 import axios from 'axios';
 
 export function fetchTweets() {
-	return axios.get('https://safe-headland-90182.herokuapp.com/')
+	console.log("FOOOfoo")
+	axios.get('https://safe-headland-90182.herokuapp.com/')
 		.then(function(response) {
 			console.log(response);
 			console.log(response.status);
 			return response.data;
+		})
+		.catch(function(error) {
+			console.log(error);
 		});
 }
 
