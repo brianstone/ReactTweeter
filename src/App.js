ce02e412a3f12d85bf7470f8e085abc7bcdb4ed3
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2016-08-25 21:13:13 -0700

    fixes all the brokeness

--- src/App.js
+++ src/App.js
@@ -1,48 +1,27 @@
 import React from 'react';
 import './App.css';
-// import { fetchTweets } from './Lib/Api'
-import axios from 'axios';
+import { fetchTweets } from './Lib/Api'
 
-// import _ from 'lodash';
-function fetchTweets() {
-  console.log("FOOOfoo")
-  axios.get('https://safe-headland-90182.herokuapp.com/')
-    .then(function(response) {
-      console.log(response);
-      console.log(response.status);
-      return response.data;
-    })
-    .catch(function(error) {
-      console.log(error);
-    });
-}
-
-class App extends React.Component {
+export default class App extends React.Component {
   constructor(props) {
     super(props);
-    // this.tweets = this.tweets.bind(this);
     this.state = {data: null};
   }
 
   componentDidMount() {
-    console.log("FooBar");
-    // console.log(fetchTweets());
-    fetchTweets().then((response) => {
-     this.setState({data: response})
-    });
-  }
-
-  tweets() {
-    console.log(this.state)
-    return this.state.data.map((tweet) => {
-      return <div>{tweet.user}: {tweet.text}</div>
-    });
+    console.log("seven")
+    fetchTweets()
+      .then((response) => {
+       this.setState({data: response})
+      })
+      .catch((error) => {
+        console.log(error)
+      });
   }
 
   render() {
-    // fetchTweets();
-    // console.log(this.state.data)
     const {data} = this.state;
+    console.log(data)
     return (
       <div className="App">
         <div className="tweeter-header">
@@ -51,11 +30,9 @@ class App extends React.Component {
         </div>
         <h1>Tweets</h1>
         {data && data.map((tweet) => (
-          <div>{tweet.user}: {tweet.text}</div>
+          <div>{tweet.user}: {tweet.tweet}</div>
         ))}
       </div>
     );
   }
 }
-
-export default App;
--- src/Lib/Api.js
+++ src/Lib/Api.js
@@ -1,15 +1,13 @@
 import axios from 'axios';
 
 export function fetchTweets() {
-	console.log("FOOOfoo")
-	axios.get('https://safe-headland-90182.herokuapp.com/')
-		.then(function(response) {
-			console.log(response);
-			console.log(response.status);
-			return response.data;
-		})
-		.catch(function(error) {
-			console.log(error);
-		});
+  return axios.get('https://safe-headland-90182.herokuapp.com/')
+    .then(function(response) {
+      return response.data;
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+
 }
 
