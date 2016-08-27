664f1f81a64d4f4cf29c0566ba3281fed504597a
Brian Stone <brianpatrickstone@gmail.com>
2016-08-26 21:07:20 -0600

    displaying tweets

--- src/App.js
+++ src/App.js
@@ -5,25 +5,10 @@ import { fetchTweets } from './Lib/Api';
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.tweets = this.tweets.bind(this);
     this.state = {data: null};
   }
 
   componentDidMount() {
-    fetchTweets().then((response) => {
-      this.setState({data: response})
-      console.log(response)
-    });
-  }
-
-  tweets() {
-    this.state.data.map(tweet => {
-      return <li>{tweet.text}</li>
-    });
-  }
-
-  render() {
-    console.log(this.state.data)
     console.log("seven")
     fetchTweets()
       .then((response) => {
@@ -43,11 +28,13 @@ export default class App extends React.Component {
           <h1 className="tweeter-h1">Search for a hashtag</h1>
           <span>#</span><input type="text" />
         </div>
-        <h1>Tweets</h1>
-        <ul>{this.tweets}</ul>
-        {data && data.map((tweet) => (
-          <div>{tweet.user}: {tweet.tweet}</div>
-        ))}
+        {/* <h1>Tweets</h1>*/}
+          {data && data.map((tweet) => (
+            <div>
+              <h1 key={tweet.id}>{tweet.user}</h1>
+              <h3>{tweet.tweet}</h3>
+            </div>
+          ))}
       </div>
     );
   }
